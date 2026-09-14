551dca2df85e96b916cf1f7c2b01345ceda71dd5
Jesse Schröder <jesse.schroder@gmail.com>
2026-09-14 11:46:36 +0200

    eerste dingen getekend voor het landschap

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,7 +1,35 @@
+let x = 100;
+let greeting = "Hello world!"
+
+let a = 20;
+let b = 10;
+
+let optellen = 30;
+let aftrekken = 10;
+let vermenigvuldigen = 200;
+let delen = 2;
+
+// 20 + 10 = 30
+optellen = a + b;
+// 20 - 10 = 10
+aftrekken = a - b;
+// 20 * 10 = 200
+vermenigvuldigen = a * b;
+// 20 / 10 = 2
+delen = a / b;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  text(x, 20, 20)
+  text(greeting, 20, 60)
+
+  text(optellen, 20, 80)
+  text(aftrekken, 20, 100)
+  text(vermenigvuldigen, 20, 120)
+  text(delen, 20, 140)
 }
--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,7 +1,60 @@
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1400, 870);
 }
 
 function draw() {
-  background(220);
+  background("lightblue");
+
+  // bergen
+  strokeWeight(1)
+  stroke("grey")
+  fill(100)
+  triangle(30, 775, 258, 250, 500, 775)
+  triangle(500, 775, 700, 350, 900, 775)
+  triangle(300, 775, 500, 450, 700, 775)
+
+  // weg met lijnen
+  strokeWeight(0)
+  fill(160)
+  rect(0,670,1400,200)
+
+  fill("white")
+  rect(-10,740,20,20,20)
+  rect(100,740,100,20,20)
+  rect(300,740,100,20,20)
+  rect(500,740,100,20,20)
+  rect(700,740,100,20,20)
+  rect(900,740,100,20,20)
+  rect(1100,740,100,20,20)
+  rect(1300,740,100,20,20)
+
+  // strookje gras
+  fill("green")
+  rect(0,620,1400,50)
+
+  // boomstammen
+  fill("brown")
+  rect(110,530,20,100)
+  rect(300,565,20,100)
+  rect(500,550,20,100)
+
+  // boombladeren
+  strokeWeight(10)
+  stroke("darkgreen")
+  fill("green")
+  circle(120,500,100)
+  circle(310,535,100)
+  circle(510,520,100)
+
+  // zon
+  fill(255, 255, 0)
+  stroke(255, 215, 140)
+  circle(130,130,150)
+
+  // stoplicht
+  strokeWeight(0)
+  fill(130)
+  rect(1200,560,10,100,50)
+  rect(1180,450,50,130)
 }
